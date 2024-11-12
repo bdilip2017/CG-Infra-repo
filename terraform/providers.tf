fd87912395d46c06b1640800c6a00622592f66f8
dilip.bondugula <dilip.bondugula@ing.com>
2024-11-12 16:39:18 +0100

    ading backend config also

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,14 @@
 provider "azurerm" {
   features {}
 }
+
+terraform {
+  required_version = ">= 0.12.24"
+  backend "azurerm" {
+
+    storage_account_name = "piogbldevsta001lrs"
+    access_key           = "__piogbldevsta001lrs-key1__"
+    container_name       = "tfstate"
+    key                  = "__rgtfstate__"
+  }
+}
